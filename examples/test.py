db94bdfa6c6376b8e80aec48800d95dd18096bf4
danielenriquez59 <deenriquez92@gmail.com>
2023-08-23 12:07:08 -0700

    test.py edits

--- examples/test.py
+++ examples/test.py
@@ -1,15 +1,10 @@
 import sys
 import os
-
-# Get the parent directory of the current working directory
 parent_dir = os.path.dirname(os.getcwd())
-# Append this path to sys.path
 sys.path.append(parent_dir)
-
 import pdb
 import pyFlightscript as pyfs
 
-# print(dir(pyfs))
 pyfs.open_fsm('test.txt', 'other.fsm')
 pyfs.stop_script('test.txt')
 pdb.set_trace()
--- test.py
+++
@@ -1,7 +0,0 @@
-import sys
-import pdb
-import pyFlightscript as pyfs
-
-pyfs.open_fsm('test.txt', 'other.fsm')
-pyfs.stop_script('test.txt')
-pdb.set_trace()
