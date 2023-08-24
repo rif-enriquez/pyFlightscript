5f73742cfaca23725ce8ae024da72649b7847745
danielenriquez59 <deenriquez92@gmail.com>
2023-08-24 12:11:57 -0700

    license

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="pyFlightscript",
     version="0.1.0",
-    description="Python package to write FlightStream scripts using python",
+    description="Python package to write FlightStream scripts using python.",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     author="Daniel Enriquez",
@@ -12,7 +12,7 @@ setup(
     packages=find_packages(),  # automatically discover and include all packages in the package directory
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU Affero General Public License v3.0",  # Modified license
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
@@ -25,3 +25,6 @@ setup(
         # include data files with the package
     },
 )
+
+# Add the copyright statement
+print("Copyright © 2023 Research In Flight")  # replace YEAR with the current year or the year of copyright
