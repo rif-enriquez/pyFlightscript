c122231eec9661bab8c0ab060397126a9c81ee39
danielenriquez59 <deenriquez92@gmail.com>
2023-08-24 11:31:47 -0700

    edits to testpy

--- examples/test.py
+++ examples/test.py
@@ -1,11 +1,9 @@
-import sys
-import os
+import sys, os
 parent_dir = os.path.dirname(os.getcwd())
 sys.path.append(parent_dir)
-import pdb
 import pyFlightscript as pyfs
 
-pyfs.fsinit.open_fsm('other.fsm')
+pyfs.fsinit.open_fsm('test.fsm')
 pyfs.script_state.display_lines()
 pyfs.script_state.write_to_file()
 
